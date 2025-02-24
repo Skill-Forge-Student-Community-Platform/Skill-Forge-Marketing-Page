dd14444a4cb8ae930ec197d8149f81de163cd0d1
lakshanFernando2003 <lakshanchanaka34@gmail.com>
2025-02-24 20:21:57 +0530

    changed settings

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [{
-        loader: '@svgr/webpack',
-        options: {
-          typescript: true,
-          dimensions: false
-        }
-      }]
-    });
-    return config;
-  }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
